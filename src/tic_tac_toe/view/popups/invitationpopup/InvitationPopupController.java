0f7ad2e0564f62f05c245065fc1a6b468c1390e6
eslam <eslam@DESKTOP-9DF40K1>
2025-01-08 01:20:46 +0200

    Server,Wating,request popup

---
+++ src/tic_tac_toe/view/popups/invitationpopup/InvitationPopupController.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tic_tac_toe.view.popups.invitationpopup;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author eslam
+ */
+public class InvitationPopupController implements Initializable {
+
+    @FXML
+    private AnchorPane mainPane;
+    @FXML
+    private Label requestlbl;
+    @FXML
+    private Button cancelBtn;
+    @FXML
+    private Button confirmBtn;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
---
+++ src/tic_tac_toe/view/popups/waitingRequestpopup/WaitingRequestPopupController.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tic_tac_toe.view.popups.waitingRequestpopup;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+/**
+ * FXML Controller class
+ *
+ * @author eslam
+ */
+public class WaitingRequestPopupController implements Initializable {
+
+    @FXML
+    private Label waitinglbl;
+    @FXML
+    private Button cancelBtn;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
