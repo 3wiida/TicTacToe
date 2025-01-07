c063872264f46a31bd516cb7595c588491f050ec
eslam <eslam@DESKTOP-9DF40K1>
2025-01-08 01:47:45 +0200

    Server,Wating,request popup

--- src/tic_tac_toe/view/available/AvailableUsersScreenController.java
+++ src/tic_tac_toe/view/available/AvailableUsersScreenController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package tic_tac_toe.view.availableUsers;
+package tic_tac_toe.view.available;
 
 import java.net.URL;
 import java.util.ResourceBundle;
