dae60af8e4f3fcaecc4bd5eed03c83d1ae30cf47
3wiida <mahmoudewida5@gmail.com>
2025-02-01 22:26:40 +0200

    add github images

--- src/tic_tac_toe/navigation/Navigator.java
+++ src/tic_tac_toe/navigation/Navigator.java
@@ -24,12 +24,13 @@ import tic_tac_toe.view.gameBoard.GameBoardFXMLController;
  * @author 3wiida
  */
 public class Navigator {
+
     private static Stage mainStage;
 
     public static void setMainStage(Stage stage) {
         mainStage = stage;
     }
-    
+
     public static Stage getMainStage() {
         return mainStage;
     }
@@ -42,24 +43,24 @@ public class Navigator {
             Scene scene = new Scene(root);
             stage.setScene(scene);
             stage.show();
-        }else{
+        } else {
             System.out.println("Can't navigate to landing screen");
         }
     }
-    
+
     public static void navigateToLandingScreen() throws IOException {
-        Parent landingRoot =  FXMLLoader.load(Navigator.class.getResource(ScreensRoutes.LANDING_SCREEN_ROUTE));
+        Parent landingRoot = FXMLLoader.load(Navigator.class.getResource(ScreensRoutes.LANDING_SCREEN_ROUTE));
         Scene landingScene = new Scene(landingRoot);
         mainStage.setScene(landingScene);
         mainStage.show();
     }
-    
+
     public static void navigateToLandingScreen(Node node) throws IOException {
-            Stage stage = (Stage) node.getScene().getWindow();
-            Parent root = FXMLLoader.load(Navigator.class.getResource(ScreensRoutes.LANDING_SCREEN_ROUTE));
-            Scene scene = new Scene(root);
-            stage.setScene(scene);
-            stage.show();
+        Stage stage = (Stage) node.getScene().getWindow();
+        Parent root = FXMLLoader.load(Navigator.class.getResource(ScreensRoutes.LANDING_SCREEN_ROUTE));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void navigateToLoginScreen(Event event) throws IOException {
@@ -98,8 +99,8 @@ public class Navigator {
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         stage.setScene(new Scene(root));
     }
-    
-     public static void naviagteToGameBoardScreen(ActionEvent event, GameModeEnum gameMode, String recordName) throws IOException {
+
+    public static void naviagteToGameBoardScreen(ActionEvent event, GameModeEnum gameMode, String recordName) throws IOException {
         FXMLLoader loader = new FXMLLoader(Navigator.class.getResource(ScreensRoutes.GAME_BOARD_SCREEN_ROUTE));
         Parent root = loader.load();
         GameBoardFXMLController controller = loader.getController();
@@ -108,13 +109,13 @@ public class Navigator {
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         stage.setScene(new Scene(root));
     }
-     
+
     public static void naviagteToGameBoardScreen(
-            ActionEvent event, 
-            GameModeEnum gameMode, 
-            String playerOneName, 
+            ActionEvent event,
+            GameModeEnum gameMode,
+            String playerOneName,
             String playerTwoName
-    ) throws IOException{
+    ) throws IOException {
         FXMLLoader loader = new FXMLLoader(Navigator.class.getResource(ScreensRoutes.GAME_BOARD_SCREEN_ROUTE));
         Parent root = loader.load();
         GameBoardFXMLController controller = loader.getController();
@@ -123,13 +124,13 @@ public class Navigator {
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         stage.setScene(new Scene(root));
     }
-    
+
     public static void naviagteToGameBoardScreen(
-            Node node, 
-            GameModeEnum gameMode, 
+            Node node,
+            GameModeEnum gameMode,
             Player opponent,
             boolean isHosting
-    ) throws IOException{
+    ) throws IOException {
         FXMLLoader loader = new FXMLLoader(Navigator.class.getResource(ScreensRoutes.GAME_BOARD_SCREEN_ROUTE));
         Parent root = loader.load();
         GameBoardFXMLController controller = loader.getController();
@@ -138,8 +139,8 @@ public class Navigator {
         Stage stage = (Stage) node.getScene().getWindow();
         stage.setScene(new Scene(root));
     }
-   
-    public static void navigateToOfflineScreen(Event event) throws IOException{
+
+    public static void navigateToOfflineScreen(Event event) throws IOException {
 
         Parent root = FXMLLoader.load(Navigator.class.getResource(ScreensRoutes.OFFLINE_SCREEN_ROUTE));
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
@@ -159,7 +160,6 @@ public class Navigator {
         return popupStage;
     }
 
-
     public static void navigateToModeSelectionScreen(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(Navigator.class.getResource(ScreensRoutes.MODE_SELECTION_SCREEN));
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
@@ -176,7 +176,7 @@ public class Navigator {
         stage.setScene(scene);
         stage.show();
     }
-    
+
     public static void navigateToOnlineScreen(Node node) throws IOException {
         FXMLLoader Loader = new FXMLLoader(Navigator.class.getResource(ScreensRoutes.ONLINE_SCREEN_ROUTE));
         Parent root = Loader.load();
@@ -224,8 +224,7 @@ public class Navigator {
         popupStage.show();
     }
 
-
-    public static void navigateToProfileScreen(Event event) throws IOException{
+    public static void navigateToProfileScreen(Event event) throws IOException {
         FXMLLoader Loader = new FXMLLoader(Navigator.class.getResource(ScreensRoutes.PROFILE_SCREEN_ROUTE));
         Parent root = Loader.load();
         Stage stage = ((Stage) ((Node) event.getSource()).getScene().getWindow());
@@ -233,17 +232,35 @@ public class Navigator {
         stage.setScene(scene);
         stage.show();
     }
-    
-      public static Stage navigateToServerShutdownScreen() throws IOException {
+
+    public static Stage navigateToServerShutdownScreen(Stage parentStage) throws IOException {
+        // Load the FXML for the server shutdown popup
         Parent root = FXMLLoader.load(Navigator.class.getResource(ScreensRoutes.SERVER_SHUTDOWN_POPUP_ROUTE));
         Scene scene = new Scene(root);
+
         Stage popupStage = new Stage();
-        popupStage.initModality(Modality.APPLICATION_MODAL);
-        popupStage.initStyle(StageStyle.UNDECORATED);
+        popupStage.initModality(Modality.APPLICATION_MODAL);  // Block interaction with parent stage
+        popupStage.initStyle(StageStyle.UNDECORATED);         // No default stage decorations
         popupStage.setScene(scene);
+
+        // Center the popup on the parent stage
+        popupStage.setOnShowing(event -> {
+            if (parentStage != null) {
+                double parentX = parentStage.getX();
+                double parentY = parentStage.getY();
+                double parentWidth = parentStage.getWidth();
+                double parentHeight = parentStage.getHeight();
+
+                double popupWidth = popupStage.getWidth();
+                double popupHeight = popupStage.getHeight();
+
+                popupStage.setX(parentX + (parentWidth - 700) / 2);
+                popupStage.setY(parentY + (parentHeight - 200) / 2);
+            }
+        });
+
         popupStage.show();
         return popupStage;
     }
-    
 
 }
--- src/tic_tac_toe/view/landing/LandingScreenController.java
+++ src/tic_tac_toe/view/landing/LandingScreenController.java
@@ -41,6 +41,7 @@ public class LandingScreenController {
     private Button btnExit;
     @FXML
     private Label lblLanding;
+
     @FXML
     void ExitClicked(ActionEvent event) {
         Platform.exit();
@@ -54,80 +55,122 @@ public class LandingScreenController {
             Logger.getLogger(LandingScreenController.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+
     @FXML
     void SignUpClicked(ActionEvent event) {
-        if(ClientSocket.isServerConnected()){
-            showAuthPopup(event);
-        }else{
-            showIPPopUp(event);
+        if (ClientSocket.isServerConnected()) {
+            showAuthPopup(event,Navigator.getMainStage());
+        } else {
+            showIPPopUp(event, Navigator.getMainStage());
         }
     }
-    private  void showAuthPopup(Event event){
+
+    private void showAuthPopup(Event event, Stage parentStage) {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource(ScreensRoutes.POPUP_Login_OR_SIGNUP_ROUTE));
             Parent root = loader.load();
             root.setStyle(
-                "-fx-background-radius: 15; " +
-                "-fx-background-color: white; " + 
-                "-fx-border-radius: 15; " +
-                "-fx-border-color: lightgray; " +
-                "-fx-border-width: 2;"
+                    "-fx-background-radius: 15; "
+                    + "-fx-background-color: white; "
+                    + "-fx-border-radius: 15; "
+                    + "-fx-border-color: lightgray; "
+                    + "-fx-border-width: 2;"
             );
-            Scene scene = new Scene(root,600,200);
+
+            Scene scene = new Scene(root, 600, 200);
             scene.setFill(Color.TRANSPARENT);
+
             LoginOrRegisterPopupController controller = loader.getController();
-            Stage  LoginOrRegisterPopup = new Stage();
-            
-            LoginOrRegisterPopup.setTitle("Login or Register?");
-            LoginOrRegisterPopup.initModality(Modality.APPLICATION_MODAL);
-            LoginOrRegisterPopup.setScene(scene);
-            LoginOrRegisterPopup.initStyle(StageStyle.TRANSPARENT);
-            
-             LoginOrRegisterPopup.showAndWait();
-            
-            int clickedAction =  controller.getAction();
-            switch(clickedAction){
-                case 1: Navigator.navigateToSignupScreen(event); break;
-                case 2: Navigator.navigateToLoginScreen(event); break;
-                case 3: Navigator.navigateToLandingScreen(event); break;
+            Stage loginOrRegisterPopup = new Stage();
+            loginOrRegisterPopup.setTitle("Login or Register?");
+            loginOrRegisterPopup.initModality(Modality.WINDOW_MODAL); // Block parent interaction
+            loginOrRegisterPopup.initStyle(StageStyle.TRANSPARENT);  // Transparent style
+            loginOrRegisterPopup.initOwner(parentStage);             // Set parent stage
+            loginOrRegisterPopup.setScene(scene);
+
+            // Center the popup relative to the parent stage
+            loginOrRegisterPopup.setOnShowing(event1 -> {
+                double parentX = parentStage.getX();
+                double parentY = parentStage.getY();
+                double parentWidth = parentStage.getWidth();
+                double parentHeight = parentStage.getHeight();
+
+                double popupWidth = loginOrRegisterPopup.getWidth();
+                double popupHeight = loginOrRegisterPopup.getHeight();
+
+                loginOrRegisterPopup.setX(parentX + (parentWidth - 600) / 2);
+                loginOrRegisterPopup.setY(parentY + (parentHeight - 200) / 2);
+            });
+
+            loginOrRegisterPopup.showAndWait();
+
+            int clickedAction = controller.getAction();
+            switch (clickedAction) {
+                case 1:
+                    Navigator.navigateToSignupScreen(event);
+                    break;
+                case 2:
+                    Navigator.navigateToLoginScreen(event);
+                    break;
+                case 3:
+                    Navigator.navigateToLandingScreen(event);
+                    break;
             }
         } catch (IOException ex) {
             Logger.getLogger(LandingScreenController.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
-     private  void showIPPopUp(ActionEvent event){
+
+    private void showIPPopUp(ActionEvent event, Stage parentStage) {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource(ScreensRoutes.SERVER_CONNECTION_POPUP_ROUTE));
             Parent root = loader.load();
-            Scene scene = new Scene(root,470,250);
+            Scene scene = new Scene(root, 470, 250);
             scene.setFill(Color.TRANSPARENT);
+
             ServerConnectionPopupController controller = loader.getController();
-            Stage  serverConnectionPopup = new Stage();
+            Stage serverConnectionPopup = new Stage();
             serverConnectionPopup.setTitle("Enter Server IP");
-            serverConnectionPopup.initModality(Modality.APPLICATION_MODAL);
+            serverConnectionPopup.initModality(Modality.WINDOW_MODAL); // Block parent interaction
+            serverConnectionPopup.initStyle(StageStyle.TRANSPARENT);  // Optional: Transparent style
+            serverConnectionPopup.initOwner(parentStage);             // Set parent stage
             serverConnectionPopup.setScene(scene);
-            serverConnectionPopup.initStyle(StageStyle.TRANSPARENT);
+
+            // Center the popup relative to the parent stage
+            serverConnectionPopup.setOnShowing(event1 -> {
+                double parentX = parentStage.getX();
+                double parentY = parentStage.getY();
+                double parentWidth = parentStage.getWidth();
+                double parentHeight = parentStage.getHeight();
+
+                double popupWidth = serverConnectionPopup.getWidth();
+                double popupHeight = serverConnectionPopup.getHeight();
+
+                serverConnectionPopup.setX(parentX + (parentWidth-450) / 2);
+                serverConnectionPopup.setY(parentY + (parentHeight-200) / 2);
+            });
+
             serverConnectionPopup.showAndWait();
+
             boolean isConnected = controller.isConnectionSuccess();
-            if(isConnected){
-                ClientSocket.recieveResponse();
-                serverConnectionPopup.setOnCloseRequest(
-                    (e)->{
-                        showAuthPopup(event);
-                    }
-                );
-                
+            if (isConnected) {
+                ClientSocket.recieveResponse();// Corrected method name
+
+                serverConnectionPopup.setOnCloseRequest(e -> {
+                    showAuthPopup(event,Navigator.getMainStage());
+                });
+
                 EventHandler<WindowEvent> closeRequestHandler = serverConnectionPopup.getOnCloseRequest();
                 if (closeRequestHandler != null) {
                     closeRequestHandler.handle(new WindowEvent(serverConnectionPopup, WindowEvent.WINDOW_CLOSE_REQUEST));
                 }
+
                 serverConnectionPopup.close();
-                         
             }
+
         } catch (IOException ex) {
             Logger.getLogger(LandingScreenController.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
+
 }
